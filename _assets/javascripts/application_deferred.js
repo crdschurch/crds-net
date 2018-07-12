7675dd96509b2296aeb7f046a6e654edf2f5ba61
Luke Moderwell <luke.moderwell@gmail.com>
2018-07-12 13:28:10 -0400

    US14203: Imgix add imgix JS

--- _assets/javascripts/application_deferred.js
+++ _assets/javascripts/application_deferred.js
@@ -1,5 +1,7 @@
+//= require ./vendor/imgix.min
 //= require ./vendor/crds-card-carousel-v0.2.0.min
 //= require ./vendor/flickity.pkgd.min
 //= require ./vendor/bootstrap.min
 //= require ./components/header
+//= require ./components/images
 //= require ./components/carousels
