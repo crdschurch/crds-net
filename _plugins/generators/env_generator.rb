1fb94d331acb6a7fe38bb239a17a8de42bc2d8e7
Michael Vezzani <mvezzani@callibrity.com>
2018-07-16 15:33:35 -0400

    Refactor to prevent multiple creations of hash data structure

--- _plugins/generators/env_generator.rb
+++ _plugins/generators/env_generator.rb
@@ -2,6 +2,13 @@ module Jekyll
   class EnvGenerator < Generator
     attr_accessor :site
 
+    ENVS = {
+      development: 'int',
+      int: 'int',
+      demo: 'demo',
+      production: 'www',
+    }
+
     def generate(site)
       @site = site
       @site.config['jekyll_env'] = ENV['JEKYLL_ENV'] || 'development'
@@ -17,13 +24,7 @@ module Jekyll
     private
 
       def env_prefix
-        envs = {
-          development: 'int',
-          int: 'int',
-          demo: 'demo',
-          production: 'www',
-        }
-        envs[@site.config['jekyll_env'].to_sym]
+        ENVS[@site.config['jekyll_env'].to_sym]
       end
 
   end
