ba1d385ac5a0d7e513792dbc697b3e7318a0b0b9
jbrammer <jbrammer@callibrity.com>
2018-08-28 13:17:34 -0400

    stripped out local dev portion of function as it won't work, simplified function to one line

--- _assets/javascripts/lib/set-redirect-url.js
+++ _assets/javascripts/lib/set-redirect-url.js
@@ -1,10 +1,3 @@
 window.onload = function() {
-    //for local development redirection
-    var domain = window.location.hostname;
-
-    if(window.location.hostname.includes('crossroads')){
-        domain = '.crossroads.net';
-    }
-    var uri = encodeURIComponent(window.location.href);
-    document.cookie = "redirectUrl=" + uri + ";domain=" + domain + ";Path=/";
+    document.cookie = "redirectUrl=" + encodeURIComponent(window.location.href) + ";domain=.crossroads.net;Path=/";
 };
