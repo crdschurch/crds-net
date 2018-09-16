1cbca5801e1997613f99dfc96a35a6da49e1290d
jasoncallibrity <jbrammer@callibrity.com>
2018-09-15 20:30:36 -0400

    added cypress.json for project setup

--- cypress/integration/homepage/homepage_spec.js
+++ cypress/integration/homepage/homepage_spec.js
@@ -1,5 +1,19 @@
-describe('Homepage', function() {
+describe('Homepage', function() {    
+
     it('Visits Crossroads Homepage', function() {
-      cy.visit('https://int.crossroads.net')
+      cy.visit('https://int.crossroads.net');
+    })
+
+    it('clicks the watch CTA button', function() {
+      cy.get('[data-automation-id="watch-cta"]').click();
+    })
+
+    it('shows the jumbotron overlay', function() {
+      cy.get('[data-automation-id="jumbotron-overlay"]').should('have.css', 'opacity', '1');
+    })
+
+    it('closes the overlay', function() {
+      cy.get('[data-automation-id="overlay-close"]').click();
+      cy.get('[data-automation-id="jumbotron-overlay"]').should('have.css', 'opacity', '0');
     })
   })
