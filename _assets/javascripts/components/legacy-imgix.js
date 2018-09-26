44e9fb9a4469839bfd7d3576a0b1768c9cbd3146
Luke Moderwell <luke.moderwell@gmail.com>
2018-09-26 15:06:14 -0400

    DE5941: Legacy Imgix Support transform image src instead of downloading old script

--- _assets/javascripts/components/legacy-imgix.js
+++ _assets/javascripts/components/legacy-imgix.js
@@ -1,28 +1,18 @@
 (function () {
-  const imgixSrc = 'https://cdn.rawgit.com/imgix/imgix.js/2.2.3/dist/imgix.min.js';
-  const imgixVersion = '2.2.3';
-  function injectLegacyScript(src, callback) {
-    const script = document.createElement('script');
-    script.onload = function() {
-      callback();
-    };
-
-    script.src = src;
-    document.getElementsByTagName('head')[0].appendChild(script);
-    console.log(`loaded legacy imgix version: ${imgixVersion}`);
+  function transformSource(el) {
+    const src = el.dataset.src;
+    el.removeAttribute('data-src');
+    el.setAttribute('ix-src', src);
   }
 
-  function legacyImageCheck() {
-    const hasLegacyImages = document.getElementsByClassName('imgix-fluid').length > 0;
-    const hasOptimizedImages = document.querySelectorAll('[data-optimize-img]').length > 0;
-    // load the script if the page only requires legacy support 
-    // and has no images using new techniques
-    if (hasLegacyImages && !hasOptimizedImages) {
-      injectLegacyScript(imgixSrc, function() {
-        imgix.fluid()
-      });
+  function findLegacyImages() {
+    // imgix 2.2.3 uses data-src instead of ix-src
+    const legacyImages = document.querySelectorAll('[data-src]');
+    console.warn(`${legacyImages.length} images might be using an outdated version of imgix`);
+    for (var i = 0; i < legacyImages.length; i += 1) {
+      transformSource(legacyImages[i]);
     }
   }
 
-  legacyImageCheck();
+  findLegacyImages();
 })();
