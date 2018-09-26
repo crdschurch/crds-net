74c6b541df37aa322eae4b5c89dff9b97645188b
Luke Moderwell <luke.moderwell@gmail.com>
2018-09-26 15:37:26 -0400

    DE5941: Legacy Imgix Support specifically look only for imgs

--- _assets/javascripts/components/legacy-imgix.js
+++ _assets/javascripts/components/legacy-imgix.js
@@ -7,10 +7,12 @@
 
   function findLegacyImages() {
     // imgix 2.2.3 uses data-src instead of ix-src
-    const legacyImages = document.querySelectorAll('[data-src]');
-    console.warn(`${legacyImages.length} images might be using an outdated version of imgix`);
-    for (var i = 0; i < legacyImages.length; i += 1) {
-      transformSource(legacyImages[i]);
+    const legacyImages = document.querySelectorAll('img[data-src]');
+    if (legacyImages.length !== 0) {
+      console.warn(`${legacyImages.length} images might be using an outdated version of imgix`);
+      for (var i = 0; i < legacyImages.length; i += 1) {
+        transformSource(legacyImages[i]);
+      }
     }
   }
 
