fc85f860117a23f0cc0653f4fe573994de127be8
jasoncallibrity <jbrammer@callibrity.com>
2018-09-28 11:53:45 -0400

    testing forms scripts

---
+++ _assets/javascripts/components/forms.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  if (document.querySelector('simple-fred')) {
+    var script = document.createElement('script');
+    script.src = '//api-int.crossroads.net/fred/js/simplefred.min.js';
+    document.head.insertBefore(script, document.head.childNodes[0]);
+  }
+});
