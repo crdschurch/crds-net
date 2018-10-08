b19fe7ebcc1c438ac113a922f14e61b822638ad3
Luke Moderwell <luke.moderwell@gmail.com>
2018-10-08 13:28:47 -0400

    US15255: Redirect Unauthenticated Users listen for custom event, redirect url set

--- _assets/javascripts/lib/set-redirect-url.js
+++ _assets/javascripts/lib/set-redirect-url.js
@@ -1,3 +1,6 @@
-window.onload = function() {
-    document.cookie = "redirectUrl=" + encodeURIComponent(window.location.href) + ";domain=.crossroads.net;path=/";
+window.onload = function () {
+  var redirectUrlSet = new Event('redirect-url-set');
+  document.cookie = "redirectUrl=" + encodeURIComponent(window.location.href) + ";domain=.crossroads.net;path=/";
+  document.dispatchEvent(redirectUrlSet);
+  window.redirectUrlSet = true;
 };
