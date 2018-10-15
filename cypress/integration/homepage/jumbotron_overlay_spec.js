509624f111fa5a0c29441df61f70485616b2f1dd
Sarah Henney <shenney@callibrity.com>
2018-10-15 17:07:18 -0400

    more git ignores and consolidated jumbotron overlay spec

--- cypress/integration/homepage/jumbotron_overlay_spec.js
+++ cypress/integration/homepage/jumbotron_overlay_spec.js
@@ -1,35 +1,19 @@
 describe('Homepage JumboTron Overlay', function() {
-
-    it('Visits Crossroads Homepage', function() {
+    before(function() {
       cy.visit('https://int.crossroads.net');
     })
 
-    it('clicks the watch CTA button', function() {
+    it('Verifies Jumbotron components are only visible when opened', function(){
       cy.get('[data-automation-id="watch-cta"]').click();
-    })
 
-    it('shows the jumbotron overlay', function() {
       cy.get('[data-automation-id="jumbotron-overlay"]').should('have.css', 'opacity', '1');
-    })
-
-    it('shows the recent service', function() {
       cy.get('[data-automation-id="recentService"]').should('have.css', 'opacity', '1');
-    })
-
-    it('shows stream service', function() {
       cy.get('[data-automation-id="stream-service"]').should('have.css', 'opacity', '1');
-    })
 
-    it('closes the overlay', function() {
       cy.get('[data-automation-id="overlay-close"]').click();
+
       cy.get('[data-automation-id="jumbotron-overlay"]').should('have.css', 'opacity', '0');
-    })
-
-    it('hides the recent service', function() {
       cy.get('[data-automation-id="recentService"]').should('have.css', 'opacity', '1');
-    })
-
-    it('hides the stream service', function() {
       cy.get('[data-automation-id="stream-service"]').should('have.css', 'opacity', '1');
     })
   })
