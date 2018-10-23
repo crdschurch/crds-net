71e06e7d76c66171d29e235aef8fe765314363c9
jasoncallibrity <jbrammer@callibrity.com>
2018-10-23 16:07:01 -0400

    Abstracted out required authentication JS into file

---
+++ _assets/javascripts/lib/auth-required.js
@@ -0,0 +1,15 @@
+document.getElementsByTagName("html")[0].style.visibility = "hidden";
+
+function redirectUnauthenticated() {
+  if (!document.cookie.includes('userId')) {
+    window.location.href = '/signin';
+  } else {
+    document.getElementsByTagName("html")[0].style.visibility = "visible";
+  }
+}
+
+document.addEventListener("redirect-url-set", redirectUnauthenticated);
+
+if (window.isRedirectUrlSet) {
+  redirectUnauthenticated();
+}
