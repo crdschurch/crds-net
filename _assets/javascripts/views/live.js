f0fe55a71dfb18b75cd704117a74c002025c06db
Luke Moderwell <luke.moderwell@gmail.com>
2018-10-24 16:00:50 -0400

    DE5963: YouTube Modal better regex

--- _assets/javascripts/views/live.js
+++ _assets/javascripts/views/live.js
@@ -26,9 +26,11 @@ function liveInit() {
   $(document).ready(function () {
     $('div.modal-video').on('show.bs.modal', function (event) {
       var origSrc = document.getElementById('modal-video-src').dataset.src;
-      var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+      var regExp = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
       var match = origSrc.match(regExp);
-      var ytId = (match && (match[7].length == 11 || match[7].length == 12)) ? match[7] : false;
+      if (match[1] !== undefined) {
+        var ytId = match[1];
+      }
       $('#modal-video-src').attr('src', 'https://www.youtube.com/embed/' + ytId);
     });
     $('div.modal-video').on('hidden.bs.modal', function (event) {
