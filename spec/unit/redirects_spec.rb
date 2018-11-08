0b4e834996c90fa19002aacbe159b2b515d74f7e
Luke Moderwell <luke.moderwell@gmail.com>
2018-11-08 15:14:10 -0500

    US15432: Contentful Redirects fix duplicate requests made via unit tests

--- spec/unit/redirects_spec.rb
+++ spec/unit/redirects_spec.rb
@@ -5,7 +5,7 @@ describe 'Redirects' do
   before do
     ENV['CONTENTFUL_ACCESS_TOKEN'] = '813af7d0df1d660fdf5e71010997a4fe621848aa225fa3e6f4fad2b50e6cdce2'
     @redirects = Redirects.new
-    @csv = CSV.read('./redirects.csv')
+    @csv = CSV.read('./spec/fixtures/redirects.csv')
   end
 
   it 'transforms status text into numeric codes' do
@@ -16,8 +16,9 @@ describe 'Redirects' do
   it 'should write rows to a csv after the first line' do
     VCR.use_cassette 'contentful/redirects' do
       current = @csv
-      @redirects.to_csv!
-      future = CSV.read('./redirects.csv')
+      tmp_csv_path = './spec/fixtures/redirects.csv'
+      @redirects.to_csv!(tmp_csv_path)
+      future = CSV.read(tmp_csv_path)
       expect(current[0] == future[0]).to eq true
       expect(current == future).to eq false
     end
