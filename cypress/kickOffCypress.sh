323bd1daff9c3f4f123030694f4178bd415f6d1f
Sarah Henney <shenney@callibrity.com>
2018-11-28 13:46:11 -0500

    experiment - better conditional syntax for production build (?)

--- cypress/kickOffCypress.sh
+++ cypress/kickOffCypress.sh
@@ -2,20 +2,20 @@
 
 #Skip testing in Prod
 #TODO will this work with $CRDS_APP_CLIENT_ENDPOINT too?
-if [[ $CRDS_APP_CLIENT_ENDPOINT == *"www.crossroads.net" ]]
+if [[ "$CRDS_APP_CLIENT_ENDPOINT" = *"www.crossroads.net" ]];
 then
     exit 0
 fi
 
 #Test the live site if we're deploying to it, else test Netlify's preview
-if [$CONTEXT == "production"]#this isn't working - what is Context's value?
+if [ "$CONTEXT" != "production" ];#try again with better syntax
 then
     test_this_URL=$CRDS_APP_CLIENT_ENDPOINT
 else
-    test_this_URL=$CRDS_APP_CLIENT_ENDPOINT #DEPLOY_URL
+    test_this_URL=$DEPLOY_URL
 fi
 
-body="{\"request\": { \"branch\":\"$HEAD\", \"config\": {\"env\": { \"baseURL\": \"$test_this_URL\", \"contentfulSpaceId\": \"$CONTENTFUL_SPACE_ID\", \"contentfulEnv\": \"$CONTENTFUL_ENV\", \"contentfulToken\": \"$CONTENTFUL_ACCESS_TOKEN\", \"mediaEndpoint\": \"$CRDS_MEDIA_ENDPOINT\", \"TEMPnetlifyContext\": \"$CONTEXT\"}}}}"
+body="{\"request\": { \"branch\":\"$HEAD\", \"config\": {\"env\": { \"baseURL\": \"$test_this_URL\", \"contentfulSpaceId\": \"$CONTENTFUL_SPACE_ID\", \"contentfulEnv\": \"$CONTENTFUL_ENV\", \"contentfulToken\": \"$CONTENTFUL_ACCESS_TOKEN\", \"mediaEndpoint\": \"$CRDS_MEDIA_ENDPOINT\", \"NetlifyContext\": \"$CONTEXT\"}}}}"
 
 
 curl -s -X POST \
