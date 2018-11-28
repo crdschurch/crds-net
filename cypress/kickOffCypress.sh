f16f098a56d0fedb93f1bb291e6161c004fb29fd
Sarah Henney <shenney@callibrity.com>
2018-11-28 13:03:51 -0500

    Logic to account for pr preview builds and merged builds

--- cypress/kickOffCypress.sh
+++ cypress/kickOffCypress.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
-body="{\"request\": { \"branch\":\"$HEAD\", \"config\": {\"env\": { \"baseURL\": \"$DEPLOY_URL\", \"contentfulSpaceId\": \"$CONTENTFUL_SPACE_ID\", \"contentfulEnv\": \"$CONTENTFUL_ENV\", \"contentfulToken\": \"$CONTENTFUL_ACCESS_TOKEN\", \"mediaEndpoint\": \"$CRDS_MEDIA_ENDPOINT\"}}}}"
-
-#Run Cypress tests against Netlify's preview build unless we're deploying to Prod
-if [ "$CRDS_APP_DOMAIN" != "www.crossroads.net" ]
+#Skip testing in Prod
+#TODO will this work with $URL too?
+if [[ "$CRDS_APP_DOMAIN" == *"www.crossroads.net" ]]
 then
-    curl -s -X POST \
-    -H "Content-Type: application/json" \
-    -H "Accept: application/json" \
-    -H "Travis-API-Version: 3" \
-    -H "Authorization: token $TRAVIS_CI" \
-    -d "$body" \
-    https://api.travis-ci.org/repo/crdschurch%2Fcrds-net/requests
- fi
+    exit 0
+fi
+
+#Test the live site if we're deploying to it, else test Netlify's preview
+if ["$CONTEXT" == "production"]
+then
+    test_this_URL = "$URL" #TODO is this int on branch?
+else
+    test_this_URL = "$DEPLOY_URL"
+fi
+
+body="{\"request\": { \"branch\":\"$HEAD\", \"config\": {\"env\": { \"baseURL\": \"$test_this_URL\", \"contentfulSpaceId\": \"$CONTENTFUL_SPACE_ID\", \"contentfulEnv\": \"$CONTENTFUL_ENV\", \"contentfulToken\": \"$CONTENTFUL_ACCESS_TOKEN\", \"mediaEndpoint\": \"$CRDS_MEDIA_ENDPOINT\"}}}}"
+
+
+curl -s -X POST \
+-H "Content-Type: application/json" \
+-H "Accept: application/json" \
+-H "Travis-API-Version: 3" \
+-H "Authorization: token $TRAVIS_CI" \
+-d "$body" \
+https://api.travis-ci.org/repo/crdschurch%2Fcrds-net/requests
