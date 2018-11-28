4e4ca13f76a2b6d3bad36364c5c43f6c4bed915a
Sarah Henney <shenney@callibrity.com>
2018-11-28 13:17:01 -0500

    Help me syntax, you're my only hope!

--- cypress/kickOffCypress.sh
+++ cypress/kickOffCypress.sh
@@ -2,17 +2,17 @@
 
 #Skip testing in Prod
 #TODO will this work with $URL too?
-if [[ "$CRDS_APP_DOMAIN" == *"www.crossroads.net" ]]
+if [[ $CRDS_APP_DOMAIN == *"www.crossroads.net" ]]
 then
     exit 0
 fi
 
 #Test the live site if we're deploying to it, else test Netlify's preview
-if ["$CONTEXT" == "production"]
+if [$CONTEXT == "production"]
 then
-    test_this_URL = "$URL" #TODO is this int on branch?
+    test_this_URL=$URL #TODO is this int on branch?
 else
-    test_this_URL = "$DEPLOY_URL"
+    test_this_URL=$DEPLOY_URL
 fi
 
 body="{\"request\": { \"branch\":\"$HEAD\", \"config\": {\"env\": { \"baseURL\": \"$test_this_URL\", \"contentfulSpaceId\": \"$CONTENTFUL_SPACE_ID\", \"contentfulEnv\": \"$CONTENTFUL_ENV\", \"contentfulToken\": \"$CONTENTFUL_ACCESS_TOKEN\", \"mediaEndpoint\": \"$CRDS_MEDIA_ENDPOINT\"}}}}"
