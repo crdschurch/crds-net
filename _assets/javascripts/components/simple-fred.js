4e8772bcfb26f25c2a9248375dc5967a07d3322f
Tate Lucas <tate.lucas@gmail.com>
2018-11-29 12:57:45 -0500

    Use @latest

--- _assets/javascripts/components/simple-fred.js
+++ _assets/javascripts/components/simple-fred.js
@@ -5,7 +5,7 @@ if (document.querySelector('simple-fred')) {
   // setup css
   styles.rel = "stylesheet";
   styles.type = "text"
-  styles.href = 'https://unpkg.com/formiojs@3.9.0/dist/formio.full.min.css';
+  styles.href = 'https://unpkg.com/formiojs@latest/dist/formio.full.min.css';
   script.src = '//api'+ prefix +'.crossroads.net/fred/js/simplefred.min.js';
   document.head.insertBefore(script, document.head.childNodes[0]);
   document.head.insertBefore(styles, document.head.childNodes[0]);
