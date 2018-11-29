37590062c9313917f410593023bd1c838a322d77
Luke Moderwell <luke.moderwell@gmail.com>
2018-11-29 14:22:10 -0500

    US15338: Form Styles write inline styles

--- _assets/javascripts/components/simple-fred.js
+++ _assets/javascripts/components/simple-fred.js
@@ -1,12 +1,28 @@
 if (document.querySelector('simple-fred')) {
-  var styles = document.createElement('link');
+  // setup FRED Script
   var script = document.createElement('script');
   var prefix = CRDS.media.prefix == '' ? '' : '-' + CRDS.media.prefix;
-  // setup css
-  styles.rel = "stylesheet";
-  styles.type = "text"
-  styles.href = 'https://unpkg.com/formiojs@3.9.0/dist/formio.full.min.css';
-  script.src = '//api'+ prefix +'.crossroads.net/fred/js/simplefred.min.js';
+  script.src = '//api' + prefix + '.crossroads.net/fred/js/simplefred.min.js';
   document.head.insertBefore(script, document.head.childNodes[0]);
-  document.head.insertBefore(styles, document.head.childNodes[0]);
+
+  // get latest styles and write inline styles
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://unpkg.com/formiojs@latest/dist/formio.full.min.css');
+  xhr.send(null);
+  xhr.onreadystatechange = function () {
+    var DONE = 4; // readyState 4 means the request is done.
+    var OK = 200; // status 200 is a successful return.
+    if (xhr.readyState === DONE) {
+      if (xhr.status === OK)
+        createInlineStyles(xhr.responseText); 
+    } else {
+      console.log('Error: ' + xhr.status);
+    }
+  }
+
+  function createInlineStyles(res) {
+    var styles = document.createElement('style');
+    styles.innerText = res;
+    document.head.insertBefore(styles, document.head.childNodes[0]);
+  }
 }
