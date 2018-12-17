3be8ff020e5552f9eb7e4e1b2bbac848fb7b13dc
Sarah Henney <shenney@callibrity.com>
2018-12-17 17:21:00 -0500

    outline of tests for happenigs section created

--- cypress/integration/homepage/home_page_happenings_spec.js
+++ cypress/integration/homepage/home_page_happenings_spec.js
@@ -1,13 +1,55 @@
 import { ContentfulApi } from '../../support/Contentful/ContentfulApi';
 
 describe('Testing the Happenings section on the Homepage without filtering', function() {
-    it('')
-//default filters by churchwide - sorted by date
+    //let promos;
+    before(function() {
+        //const content = new ContentfulApi();
+        //promos = content.retrievePromosByLocation();
 
+        cy.visit('/');
+    })
+
+    it('Tests "Churchwide" filter is selected by default', function(){
+        cy.get('[data-automation-id="happenings-dropdown"]').find('[data-current-label]').should('have.text', 'Churchwide');
+    })
+
+    it('Tests user can filter by any Target Audience', function(){
+        //get list of expected target audiences
+        //make sure all are listed infilter
+    })
 })
 
 describe('Testing the filtering functionality for the Homepage Happenings section', function() {
-    it('')
+    let promos;
+    before(function() {
+        const content = new ContentfulApi();
+        promos = content.retrievePromosByLocation();
+
+        cy.visit('/');
+    })
+
+    it.only('Tests selecting the "Oakley" filter displays only Oakley promos', function(){
+        const oakleyPromos = promos["Oakley"];
+        cy.log(oakleyPromos.length);//debug
+        selectFilter("Oakley");
+
+        //TODO change this to a data-automation-id
+        cy.get('#section-what-s-happening > [data-filter*="Churchwide"]').then($cardList => {
+            expect($cardList).lengthOf(oakleyPromos.length);
+        })
+        //see if can do this by element content
+    })
+
+    function selectFilter(audience){
+        cy.get('[data-automation-id="happenings-dropdown"]').as("filter");
+        cy.get('@filter').click();//open
+        cy.get('@filter').find(`[data-filter-select="${audience}"]`).click();//TODO selecting is legit throwing a console error that doesn't affect the page.
+        // Try raising a defect but catching it here
+    }
+
+    it('Tests filtered results are ordered by date (newest to oldest)', function(){
+        //may need to sort promos by title too if dates are identical
+    })
     //verify expected target audiences can be selected
 //sort by oakley - results should only include oakley promos sorted by date
 
--- cypress/support/Contentful/Models/PromoModel.js
+++ cypress/support/Contentful/Models/PromoModel.js
@@ -1,39 +1,62 @@
 import { Formatter } from '../../Formatter'
+//import { callbackify } from 'util';
 //promosByLocation class
 
 export class PromosByLocation{
     storePromosByLocation(response){
         const itemList = response.items;
 
-        //create a promo for each
         for (let i = 0; i < itemList.length; i++){
             let promo = new PromoModel(itemList[i]);
-            //add TAs to list
-            promo.target_audience().forEach(ta => {
-                this[ta].push(promo);
-            });
+
+            if(promo.target_audience !== undefined){
+                this._addPromoToAudienceLists(promo.target_audience, promo);
+            }
+        }
+    }
+
+    getPromosSortedByDate(promoName){
+        const list = this[promoName];
+        if(list === undefined){
+            return;
         }
 
-        //for each location, create property in PBL with location name and add PM to that list
+        const sortedPromos = list.sort((a,b) => {
+            return (new Date(b.publishedAt) - new Date(a.publishedAt));
+        })
+        return sortedPromos;
     }
-    //
-}
 
-//promos model
+    _addPromoToAudienceLists(audienceList, promo){
+        for(let i = 0; i < audienceList.length; i++){
+            let curAudience = audienceList[i];
+
+            if(this[curAudience] === undefined){
+                this[curAudience] = [];
+            }
+            this[curAudience].push(promo);
+        }
+    }
+}
 
 export class PromoModel {
     constructor(responseItem){
-        this._name = responseItem.fields.name;
+        this._title = responseItem.fields.title;
         this._link = responseItem.fields.link_url;
         this._target_audience = responseItem.fields.target_audience;
+        this._published_at = responseItem.fields.published_at;
     }
 
-    get name(){
-        return this._name;
+    get title(){
+        return this._title;
     }
 
     get link(){
         return this._link;
+    }
+
+    get publishedAt(){
+        return this._published_at;
     }
 
     get target_audience(){
