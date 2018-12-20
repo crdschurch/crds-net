d6d6c9510f96c96739b7167f9dd069feee984688
Taylor C. MacDonald <tcmacdonald@gmail.com>
2018-12-20 15:04:35 -0500

    DEXXXX Frozen NilClass Nuclear option: disable all cloudsearch stuff unless explicitly enabled

--- _plugins/hooks/cloudsearch.rb
+++ _plugins/hooks/cloudsearch.rb
@@ -6,35 +6,33 @@ if enabled
   Jekyll::Hooks.register :documents, :post_render do |doc|
     @client.add_document(doc)
   end
-end
 
-Jekyll::Hooks.register :site, :post_write do |site|
-  Jekyll.logger.info('AWS Cloudsearch:', "#{@client.docs.size} static pages")
+  Jekyll::Hooks.register :site, :post_write do |site|
+    Jekyll.logger.info('AWS Cloudsearch:', "#{@client.docs.size} static pages")
 
-  system_pages = './system-pages.json'
-  if File.exists?(system_pages)
-    json = JSON.parse(File.read(system_pages))
-    docs = json.dig('systemPages').collect do |page|
-      url = page.dig('uRL')
-      if url.present?
-        doc = {
-          id: "SP_#{page.dig('id')}",
-          type: "add",
-          fields: {
-            title: page.dig('title'),
-            content: ::Nokogiri::HTML(page.dig('description'), &:noblanks).text,
-            link: page.dig('uRL'),
-            type: 'SystemPage'
+    system_pages = './system-pages.json'
+    if File.exists?(system_pages)
+      json = JSON.parse(File.read(system_pages))
+      docs = json.dig('systemPages').collect do |page|
+        url = page.dig('uRL')
+        if url.present?
+          doc = {
+            id: "SP_#{page.dig('id')}",
+            type: "add",
+            fields: {
+              title: page.dig('title'),
+              content: ::Nokogiri::HTML(page.dig('description'), &:noblanks).text,
+              link: page.dig('uRL'),
+              type: 'SystemPage'
+            }
           }
-        }
-        @client.docs.push(doc)
-        doc
-      end
-    end.compact
-    Jekyll.logger.info('AWS Cloudsearch:', "#{docs.size} system pages")
-  end
+          @client.docs.push(doc)
+          doc
+        end
+      end.compact
+      Jekyll.logger.info('AWS Cloudsearch:', "#{docs.size} system pages")
+    end
 
-  if enabled
     Jekyll.logger.info('AWS Cloudsearch:', "Writing... #{@client.send(:filename)}")
     @client.instance_variable_set('@site', site)
     @client.write
