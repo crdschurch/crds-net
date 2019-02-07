a28ff9bd9a1f4ae9633cc02ca2910aace1ba4311
Sarah Henney <shenney@callibrity.com>
2019-02-07 11:28:51 -0500

    Handle jumbotron background load better Handle profile menu open better

--- cypress/integration/external_repo_media/current_series_page_spec.js
+++ cypress/integration/external_repo_media/current_series_page_spec.js
@@ -1,18 +1,18 @@
 import { ContentfulApi } from '../../Contentful/ContentfulApi';
 import { ContentfulElementValidator as Element } from '../../Contentful/ContentfulElementValidator';
 
-describe('Tesing the Media/Series/[Current Series] page:', function(){
+describe('Tesing the Media/Series/[Current Series] page:', function () {
   let currentSeries;
-  before(function() {
+  before(function () {
     const seriesList = new ContentfulApi().retrieveSeriesManager();
 
-    cy.wrap({seriesList}).its('seriesList.currentSeries').should('not.be.undefined').then(() => {
+    cy.wrap({ seriesList }).its('seriesList.currentSeries').should('not.be.undefined').then(() => {
       currentSeries = seriesList.currentSeries;
       cy.visit(`${Cypress.env('CRDS_MEDIA_ENDPOINT')}/series/${currentSeries.slug.text}`);
     });
   });
 
-  it('The jumbotron image and background image should match Contentful', function() {
+  it('The jumbotron image and background image should match Contentful', function () {
     //Current series image
     cy.get('.jumbotron-content').as('currentSeries');
     Element.shouldHaveImgixImageFindImg('currentSeries', currentSeries.image);
@@ -20,12 +20,11 @@ describe('Tesing the Media/Series/[Current Series] page:', function(){
     //Large jumbotron image
     cy.get('.jumbotron').as('jumbotron');
     cy.get('@jumbotron').should('be.visible');
-    cy.get('@jumbotron').then(($jumbotronBackground) => {
-      if(currentSeries.backgroundImage.isRequiredOrHasContent){
-        expect($jumbotronBackground).to.have.attr('style').contains(currentSeries.backgroundImage.id);
-      } else if (currentSeries.image.isRequiredOrHasContent){
-        expect($jumbotronBackground.find('div')).to.have.attr('style').contains(currentSeries.image.id);
-      }
-    });
+
+    if (currentSeries.backgroundImage.isRequiredOrHasContent) {
+      cy.get('@jumbotron').should('have.attr', 'style').and('contain', currentSeries.backgroundImage.id);
+    } else if (currentSeries.image.isRequiredOrHasContent) {
+      cy.get('@jumbotron').find('div').should('have.attr', 'style').and('contain', currentSeries.image.id);
+    }
   });
 });
--- cypress/integration/sharedHeader/support/my_profile_menu.js
+++ cypress/integration/sharedHeader/support/my_profile_menu.js
@@ -1,9 +1,11 @@
 import { RouteValidator } from '../../../support/RouteValidator';
 
-export function forceOpenProfileDropdown(){
-  cy.get('#crds-shared-header-desktop-toggle').parent().as('myProfileMenu');
+export function forceOpenProfileDropdown() {
+  cy.get('#crds-shared-header-desktop-toggle').find('.profile-picture').as('myProfileIcon');
+  cy.get('@myProfileIcon').should('be.visible');
 
-  cy.get('@myProfileMenu').invoke('attr', 'class').then(($classValues) =>{
+  cy.get('#crds-shared-header-desktop-toggle').parent().as('myProfileMenu');
+  cy.get('@myProfileMenu').invoke('attr', 'class').then(($classValues) => {
     let newClass = `${$classValues} open`;
     cy.get('@myProfileMenu').invoke('attr', 'class', newClass);
   });
@@ -11,7 +13,7 @@ export function forceOpenProfileDropdown(){
   cy.get('#crds-shared-header-desktop-toggle').invoke('attr', 'aria-expanded', 'true');
 }
 
-export function openProfileClickLinkAndConfirmLoad(alias){
+export function openProfileClickLinkAndConfirmLoad(alias) {
   forceOpenProfileDropdown();
   cy.get(`@${alias}`).should('be.visible').and('have.attr', 'href').and('not.be.empty');
   cy.get(`@${alias}`).click();
