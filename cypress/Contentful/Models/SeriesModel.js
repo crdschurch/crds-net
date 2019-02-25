2e3a8870f965baac80778096e436d3a46dfc48f0
Sarah Henney <shenney@callibrity.com>
2019-02-25 11:41:55 -0500

    cleanup

--- cypress/Contentful/Models/SeriesModel.js
+++ cypress/Contentful/Models/SeriesModel.js
@@ -3,31 +3,6 @@ import { ImageField } from '../Fields/ImageField';
 import { DateField } from '../Fields/DateField';
 
 export class SeriesManager {
-  // storeCurrentSeries2(response) {
-  //   const itemList = response.items;
-  //   const assetList = response.includes.Asset;//TODO trickle down and add to other models
-
-  //   const currentSeries = itemList.find(s => {
-  //     return (Date.now() >= new Date(s.fields.published_at));
-  //   });
-
-  //   this._current_series = new SeriesModel(currentSeries.fields, assetList);
-  // }
-
-  // //This is the series for the current message (messages are in the videos property)
-  // storeRecentSeriesWithMessage(response) {
-  //   const itemList = response.items;
-  //   const assetList = response.includes.Asset;//TODO trickle down and add to other models
-
-  //   const publishedSeries = itemList.find(s => {
-  //     let isPublished = (Date.now() >= new Date(s.fields.published_at));
-  //     let hasVideoMessage = s.fields.videos === undefined ? false : true;
-  //     return isPublished && hasVideoMessage;
-  //   });
-
-  //   this._current_series = new SeriesModel(publishedSeries.fields, assetList);
-  // }
-
   //Stores the current series and the most recent series with a message
   storeCurrentSeries(response) {
     const itemList = response.items;
