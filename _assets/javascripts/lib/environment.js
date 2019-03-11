41301cb51471f3490e43e1d346c6ee2486385f1d
Taylor C. MacDonald <tcmacdonald@gmail.com>
2019-03-10 21:44:08 -0400

    DE6358 Environments Add support for environmentally specific hrefs

---
+++ _assets/javascripts/lib/environment.js
@@ -0,0 +1,40 @@
+window.CRDS = window.CRDS || {};
+
+CRDS.Environment = class Environment {
+  constructor() {
+    this.debug = false;
+    this.links();
+  }
+
+  links() {
+    this.log("links()");
+    if (this.env() == "int" || this.env() == "demo") {
+      $("a[data-href-int], a[data-href-demo]").each(
+        function(i, el) {
+          const href = $(el).data(`href-${this.env()}`);
+          $(el).attr("href", href);
+        }.bind(this)
+      );
+    }
+  }
+
+  env() {
+    const mappings = {
+      development: "int",
+      int: "int",
+      demo: "demo",
+      release: "demo"
+    };
+    return mappings[CRDS.env["environment"]];
+  }
+
+  log(str) {
+    if (this.debug) {
+      console.log(str);
+    }
+  }
+};
+
+$(document).ready(function() {
+  new CRDS.Environment();
+});
