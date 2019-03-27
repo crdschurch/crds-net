fc10cbe9a702ba6935ea73bc2a51f1607b397a2e
Taylor C. MacDonald <tcmacdonald@gmail.com>
2019-03-26 22:35:33 -0400

    DE6590 Imgix Refactor script invocation of ImgixOptimizer

--- _assets/javascripts/components/images.js
+++ _assets/javascripts/components/images.js
@@ -1,3 +1,14 @@
-$(document).ready(function(params) {
+$(document).ready(function() {
   new Imgix.Optimizer();
+
+  var n = setInterval(function(){
+    var imgs = $('[data-imgix-img-processed]'),
+        bgs = $('[data-imgix-bg-processed]');
+
+    if(imgs.length == 0 && bgs.length == 0) {
+      $(window).trigger('resize')
+    } else {
+      clearInterval(n);
+    }
+  }.bind(this), 250);
 });
