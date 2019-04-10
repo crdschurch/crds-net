a6d4473a8565867fe4a966573296d8f22596ed17
tatelucas <tate.lucas@gmail.com>
2019-04-10 11:16:10 -0400

    tweak js

--- _assets/javascripts/vendor/ash-wednesday-experience.js
+++ _assets/javascripts/vendor/ash-wednesday-experience.js
@@ -2,38 +2,22 @@ $(document).ready(function () {
     $('#fullpage').fullpage({
       'verticalCentered': false,
       slidesNavigation: true,
-      controlArrows: false,
-      slideSelector: '.slide-ash',
-      afterLoad: function (anchorLink, index) {
-        //console.log("afterLoad--" + "anchorLink: " + anchorLink + " index: " + index );
-        if (index == 2) {
-          $(".ash-audio").show();
-          //$(".ash-audio-controls").trigger( "click" );
-          $.fn.fullpage.setMouseWheelScrolling(true);
-          $.fn.fullpage.setAllowScrolling(true);
-        }
-      },
-      afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
-        //console.log("afterSlideLoad--" + "anchorLink: " + anchorLink + " index: " + index + " slideAnchor: " + slideAnchor + " slideIndex: " + slideIndex);
-        if (slideIndex == 7) {
-          $(".fp-controlArrow.fp-next").hide();
-        } else {
-          $(".fp-controlArrow.fp-next").show();
-        }
-      },
+      controlArrows: true,
+      slideSelector: '.slide-ash-old',
+      navigation: true,
     });
 
-    $(".opening-scroll-down").click(function () {
-      $.fn.fullpage.moveTo(2, 0);
-    });
+    // $(".opening-scroll-down").click(function () {
+    //   $.fn.fullpage.moveTo(2, 0);
+    // });
 
-    $(".move-right").click(function () {
-      $.fn.fullpage.moveSlideRight();
+    $(".next-section").click(function () {
+      $.fn.fullpage.moveSectionDown();
     });
-
+    //
     $.fn.fullpage.setMouseWheelScrolling(false);
     $.fn.fullpage.setAllowScrolling(false);
-
+    //
     $(".fp-wrapper").fadeTo("slow", 1.0);
 
   });
