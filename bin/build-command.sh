c8e37d95d70280f8449c12f454ca59e59353be48
j-brammer <jbrammer@callibrity.com>
2019-04-25 14:04:06 -0400

    updated build command output and logzio output

--- bin/build-command.sh
+++ bin/build-command.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Channel that is output to logfile is stderr only
+# std out is output to /dev/null
 # Ships logs to logz.io if there is a failure
 
-(bundle exec rspec 2> buildlog.txt &&
-./bin/contentful-redirects 2> buildlog.txt &&
-./bin/netlify-redirector 2> buildlog.txt &&
-bundle exec jekyll crdsnope 2> buildlog.txt &&
-bundle exec jekyll contentful -f 2> buildlog.txt &&
-bundle exec jekyll build -- --update-search-index 2> buildlog.txt &&
-bash ./cypress/kickOffCypress.sh 2> buildlog.txt &&
-./bin/health-check.sh "we are crossroads") ||
+(bundle exec rspec 1> /dev/null 2>> buildlog.txt &&
+./bin/contentful-redirects 1> /dev/null 2>> buildlog.txt &&
+./bin/netlify-redirector 1> /dev/null 2>> buildlog.txt &&
+bundle exec jekyll crdsnope 1> /dev/null 2>> buildlog.txt &&
+bundle exec jekyll contentful -f 1> /dev/null 2>> buildlog.txt &&
+bundle exec jekyll build -- --update-search-index 1> /dev/null 2>> buildlog.txt &&
+bash ./cypress/kickOffCypress.sh 1> /dev/null 2>> buildlog.txt &&
+./bin/health-check.sh "we are crossroads" 1> /dev/null 2>> buildlog.txt) ||
 ./bin/logzio.sh
--- bin/logzio.sh
+++ bin/logzio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -T buildlog.txt http://listener.logz.io:8021/file_upload/${LOGZIO_API_KEY}/crds-net
+curl -T buildlog.txt http://listener.logz.io:8021/file_upload/${LOGZIO_API_KEY}/${APPLICATION_NAME}
 rm -rf buildlog.txt
 
 exit 1
