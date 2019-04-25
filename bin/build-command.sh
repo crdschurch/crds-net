5417d2831ab8123f0277c39c100e1ffcca443313
j-brammer <jbrammer@callibrity.com>
2019-04-25 17:54:55 -0400

    updated build commands

--- bin/build-command.sh
+++ bin/build-command.sh
@@ -3,12 +3,14 @@
 # std out is output to /dev/null
 # Ships logs to logz.io if there is a failure
 
-(bundle exec rspec 1> /dev/null 2>> buildlog.txt &&
-./bin/contentful-redirects 1> /dev/null 2>> buildlog.txt &&
-./bin/netlify-redirector 1> /dev/null 2>> buildlog.txt &&
-bundle exec jekyll crds 1> /dev/null 2>> buildlog.txt &&
-bundle exec jekyll contentful -f 1> /dev/null 2>> buildlog.txt &&
-bundle exec jekyll build -- --update-search-index 1> /dev/null 2>> buildlog.txt &&
-bash ./cypress/kickOffCypress.sh 1> /dev/null 2>> buildlog.txt &&
-./bin/health-check.sh "we are crossroads" 1> /dev/null 2>> buildlog.txt) ||
+{
+  bundle exec rspec &&
+  ./bin/contentful-redirects &&
+  ./bin/netlify-redirector &&
+  bundle exec jekyll crdsnope &&
+  bundle exec jekyll contentful -f &&
+  bundle exec jekyll build -- --update-search-index &&
+  bash ./cypress/kickOffCypress.sh &&
+  ./bin/health-check.sh "we are crossroads"
+} 2>&1 | tee buildlog.txt
 ./bin/logzio.sh
