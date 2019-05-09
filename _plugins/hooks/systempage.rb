28b6df5ef437d60062f7d5556407e0e58ab1a515
Luke Moderwell <luke.moderwell@gmail.com>
2019-05-09 14:20:07 -0400

    add comment for FB sizing

--- _plugins/hooks/systempage.rb
+++ _plugins/hooks/systempage.rb
@@ -19,6 +19,7 @@ Jekyll::Hooks.register :site, :post_read do |site|
 
   def imgixify(item)
     item['image']['url'] = item['image']['url'].sub(/#{ENV['IMGIX_SRC']}/, ENV['IMGIX_DOMAIN'])
+    # per FB's best practices for meta image
     item['image']['url'] = "#{item['image']['url']}?auto=format\&w=1200\&h=630\&fit=crop"
   end
 
@@ -40,6 +41,6 @@ Jekyll::Hooks.register :site, :post_read do |site|
 
     binarize_legacy_styles(field)
   }
-
+  
   write_file(systemPages)
 end
