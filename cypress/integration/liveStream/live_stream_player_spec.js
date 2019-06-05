511076a29e1a9c920263928d400715f0e48780b3
Sarah Henney <shenney@callibrity.com>
2019-06-05 11:27:19 -0400

    Added current stream fixture for new scheduler and hide rollcall

--- cypress/integration/liveStream/live_stream_player_spec.js
+++ cypress/integration/liveStream/live_stream_player_spec.js
@@ -1,8 +1,16 @@
+function hideRollCall(){
+  localStorage.setItem('crds-roll-call-state', 'dismissed'); //this works
+  cy.get('.roll-call').invoke('hide');
+}
+
 describe('Tests the /live/stream page displays the expected player', function () {
-  it('Displays the Bitmovin player when the message has a Bitmovin URL', function () {
+  it.only('Displays the Bitmovin player when the message has a Bitmovin URL', function () {
     cy.server();
-    cy.route('/broadcaster/crossr30e3/broadcasts/upcomingPlusCurrent', 'fixture:fakeCurrentStream_streamspot.json');
-    cy.visit('/live/stream');
+    cy.route('/int/streamSchedule', 'fixture:current_stream_custom.json');//NOTE this fixture just needs some object in the "current" property - dates don't seem to matter
+
+
+    cy.visit('https://deploy-preview-882--int-crds-net.netlify.com/live/stream');
+    hideRollCall();
     //Verify iframe contains the Bitmovin player
     //If we are able to spoof the response with a real message, verify the message autoplays AND fires off the expected analytics events.
   });
@@ -16,4 +24,7 @@ describe('Tests the /live/stream page displays the expected player', function ()
     //Will there be anything else displayed?
     //How will this work with autoplay? With Analytics?
   });
+
+
+  //This works https://deploy-preview-882--int-crds-net.netlify.com/live/stream
 });
