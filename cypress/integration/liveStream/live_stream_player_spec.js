e554418a05b6fe3b7f40305eb93a278ef5c019e5
Sarah Henney <shenney@callibrity.com>
2019-06-12 14:17:40 -0400

    Creted class to generate live stream schedules Updated live stream tests to point to int

--- cypress/integration/liveStream/live_stream_player_spec.js
+++ cypress/integration/liveStream/live_stream_player_spec.js
@@ -1,11 +1,7 @@
 import { MessageQueryManager } from '../../Contentful/QueryManagers/MessageQueryManager';
 import { AmplitudeEventChecker } from './helpers/AmplitudeEventChecker';
 import { BitmovinPlayer } from './helpers/BitmovinPlayer';
-
-//TODO remove this once US17311 is comitted
-Cypress.config({
-  baseUrl: 'https://5cfa5f8b978a0d000a347d13--int-crds-net.netlify.com'
-});
+import { StreamScheduleGenerator } from './helpers/ScheduleGenerator';
 
 function hideRollCall() {
   localStorage.setItem('crds-roll-call-state', 'dismissed');
@@ -20,19 +16,22 @@ function getYoutubeId(youtubeURL) {
 
 describe('Tests the /live/stream page displays the expected player', function () {
   let latestMessage;
+  let fakeSchedule;
   before(function () {
     const mqm = new MessageQueryManager();
     mqm.fetchLatestMessage().then(result => {
       latestMessage = result;
     });
+
+    fakeSchedule = new StreamScheduleGenerator().streamStartingNow;
   });
 
   it('Displays the Bitmovin player or fallback Youtube player', function () {
     cy.server();
-    cy.route('/int/streamSchedule', 'fixture:current_stream_custom.json');//NOTE this fixture just needs some object in the "current" property - dates don't seem to matter
+    cy.route('/int/streamSchedule', fakeSchedule);
     cy.route('manifest.m3u8').as('bitmovinManifest');
 
-    cy.visit('/live/stream-test/');
+    cy.visit('/live/stream/');
     hideRollCall();
 
     if (latestMessage.bitmovinURL.hasValue) {
@@ -43,12 +42,11 @@ describe('Tests the /live/stream page displays the expected player', function ()
         expect(manifest.url).to.eq(latestMessage.bitmovinURL.text);
       });
     } else if (latestMessage.youtubeURL.hasValue) {
-      //TODO Uncomment and test when US17317 is committed
-      // cy.get('#js-media-video').as('youtubePlayer').should('be.visible');
-      // cy.get('#VideoManager').as('bitmovinPlayer').should('not.exist');
+      cy.get('#js-media-video').as('youtubePlayer').should('be.visible');
+      cy.get('#VideoManager').as('bitmovinPlayer').should('not.exist');
 
-      // const youtubeId = getYoutubeId(latestMessage.youtubeURL.text);
-      // cy.get('@youtubePlayer').should('have.attr', 'video-id', youtubeId);
+      const youtubeId = getYoutubeId(latestMessage.youtubeURL.text);
+      cy.get('@youtubePlayer').should('have.attr', 'video-id', youtubeId);
     } else {
       assert.isTrue(false, 'Latest message should have a Bitmovin URL or YouTube URL, but it does not');
     }
@@ -56,14 +54,14 @@ describe('Tests the /live/stream page displays the expected player', function ()
 
   it('Autoplays the stream muted with subtitles', function () {
     cy.server();
-    cy.route('/int/streamSchedule', 'fixture:current_stream_custom.json');
+    cy.route('/int/streamSchedule', fakeSchedule);
     const ampEvents = new AmplitudeEventChecker();
 
-    cy.visit('/live/stream-test/'); //stream-test should be valid in builds from now on
+    cy.visit('/live/stream/');
     hideRollCall();
 
     if (latestMessage.bitmovinURL.hasValue) {
-      //ampEvents.waitForVideoEvents(['VideoStarted'], latestMessage.bitmovinURL.text, 3); //TODO Uncomment and test when US17311 is committed
+      ampEvents.waitForVideoEvents(['VideoStarted'], latestMessage.bitmovinURL.text, 3);
 
       const player = new BitmovinPlayer();
       player.waitUntilBuffered().then(() => {
@@ -71,7 +69,7 @@ describe('Tests the /live/stream page displays the expected player', function ()
         player.verifyPlayerMuted();
       });
     } else if (latestMessage.youtubeURL.hasValue) {
-      //ampEvents.waitForVideoEvents(['VideoStarted'], latestMessage.youtubeURL.text, 3); //TODO Uncomment and test when US17311 is committed
+      ampEvents.waitForVideoEvents(['VideoStarted'], latestMessage.youtubeURL.text, 3);
     } else {
       assert.isTrue(false, 'Latest message should have a Bitmovin URL or YouTube URL, but it does not');
     }
