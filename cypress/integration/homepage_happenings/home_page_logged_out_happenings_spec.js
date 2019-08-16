5737635436a697ec69c6d99b66016b9b0445dbfa
Sarah Henney <shenney@callibrity.com>
2019-08-16 16:43:54 -0400

    Testing happening card components

--- cypress/integration/homepage_happenings/home_page_logged_out_happenings_spec.js
+++ cypress/integration/homepage_happenings/home_page_logged_out_happenings_spec.js
@@ -1,9 +1,12 @@
 import { PromoQueryManager } from 'crds-cypress-contentful';
+import { ExtendedPromoEntry } from "../../Contentful/ExtendedPromoEntry";
+import { Formatter } from '../../support/Formatter';
 
 describe('Tests Happening section with default filter', function () {
   const defaultAudience = 'Churchwide';
   let loadStatus = {};
   let pqm = new PromoQueryManager();
+
   before(function () {
     cy.visit('/', {
       onBeforeLoad: (win) => {
@@ -57,6 +60,33 @@ describe('Tests Happening section with default filter', function () {
       cy.get('@happeningsRoot').should('have.prop', 'shadowRoot').then(root => {
         const selector = root.querySelector('[data-automation-id="happenings-filter"]').querySelector(`option[value="${audience}"]`);
         expect(selector).to.eq.null;
+      });
+    });
+  });
+
+  it('Checks promo cards have expected compomenents', function() {
+    cy.get('@happeningsRoot').should('have.prop', 'shadowRoot').then(root => {
+      const firstCard = root.querySelector('[data-automation-id="happenings-cards"]').querySelector('.card');
+      const imageBlock = firstCard.querySelector('a[class="relative"]');
+      const cardHref = imageBlock.href;
+
+      const promosQuery = `fields.link_url=${cardHref}`;
+      pqm.entryConstructor = ExtendedPromoEntry;
+      pqm.getListOfEntries(promosQuery).then(promos => {
+        const firstPromo = promos[0];
+
+        expect(cardHref).to.eq(firstPromo.linkURL.text);
+
+        const imageElement = imageBlock.querySelector('img');
+        expect(imageElement.alt).to.eq(firstPromo.title.text);
+        expect(imageElement.src).to.include(firstPromo.imageLink.id);
+
+        const textBlock = firstCard.querySelector('.card-title > a');
+        expect(textBlock.href).to.eq(firstPromo.linkURL.text);
+        expect(textBlock.textContent).to.eq(firstPromo.title.text);
+
+        const cardText = firstCard.querySelector('.card-text');
+        expect(Formatter.normalizeText(cardText.innerHTML)).to.eq(firstPromo.description.unformattedText);
       });
     });
   });
