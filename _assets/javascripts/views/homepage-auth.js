41aa62c7a3ca3d573f7d7679048eb364533c3cbd
j-brammer <jbrammer@callibrity.com>
2019-08-19 13:16:03 -0400

    removed url manipulation, added search excluded to hpli

--- _assets/javascripts/views/homepage-auth.js
+++ _assets/javascripts/views/homepage-auth.js
@@ -36,10 +36,6 @@ function auth() {
 
 function handleLoggedInState(path) {
   if (path === 'h') { // h is the location of the new logged in homepage
-    let stateObj = {
-      h: "",
-    };
-    history.pushState(stateObj, "Crossroads", "/");
     showPage();
   } else {
     window.location.href = '/h';
