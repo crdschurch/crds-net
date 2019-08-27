5ba9379177c555d6b8061341b24741a73273acbd
Josh Byron <joshhbyron@gmail.com>
2019-08-27 11:06:48 -0400

    US17927-onsite-groups - Resolves requested changes

--- _plugins/filters/in_groups_of.rb
+++ _plugins/filters/in_groups_of.rb
@@ -3,7 +3,7 @@ require 'active_support/core_ext/array/grouping'
 module Jekyll
   module InGroupsOf
     def in_groups(array, group_count)
-      return array.in_groups_of(group_count).map{ |row| row.compact }
+      return array.in_groups_of(group_count, false)
     end
   end
 end
