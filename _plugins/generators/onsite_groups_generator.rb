935f99444cfc106abd824a9be4138b1ca4fe0a82
Taylor C. MacDonald <tcmacdonald@gmail.com>
2019-09-09 11:47:07 -0400

    Revert "[hotfix] Onsite Groups"
    This reverts commit b03a948e279dc0b6adab793a27627cf684bba7e1.

--- _plugins/generators/onsite_groups_generator.rb
+++ _plugins/generators/onsite_groups_generator.rb
@@ -2,9 +2,6 @@ module Jekyll
   class OnsiteGroupsGenerator < Generator
 
     def generate(site)
-      # Temporarily disable new page generation
-      return
-
       # Get all meetings documents from Jekyll collections
       meetings = site.
         collections['onsite_group_meetings'].docs
