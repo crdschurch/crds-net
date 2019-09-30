a1a5b8f26103f2f7a5d435e8a5bcacabe3211f57
Jimmy Janssen <jjanssen@CSMBP0010-JJ.local>
2019-09-30 14:35:48 -0400

    removing splat

--- lib/crds/redirects.rb
+++ lib/crds/redirects.rb
@@ -66,7 +66,7 @@ class Redirects
     def login_attrs(item) 
       [
         item.dig('fields', 'permalink'),
-        'https://${env:CRDS_SIGNIN_ENDPOINT}/signin/:splat',
+        'https://${env:CRDS_SIGNIN_ENDPOINT}/signin/',
         '401!'
       ]
     end
