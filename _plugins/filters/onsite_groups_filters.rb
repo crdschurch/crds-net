e0dc134d94e8b21a46e5e725f6e1006238035ecc
tcmacdonald <tcmacdonald@gmail.com>
2020-03-11 22:39:03 -0400

    US18996 OSG Rescue NilClass errors

--- _plugins/filters/onsite_groups_filters.rb
+++ _plugins/filters/onsite_groups_filters.rb
@@ -4,15 +4,14 @@ module Jekyll
     def onsite_groups_for_category(category, perSlice = 2)
       groups = site.collections['onsite_groups'].docs.
         select{|g| g.data.keys.include?('category') }.
-        select{|g| g['category']['slug'] == category['slug'] }
+        select{|g| g.data.dig('category','slug') == category['slug'] }
       groups.each_slice(perSlice).to_a
     end
 
     def locations_with_onsite_groups(collection)
       locations = site.collections['onsite_group_meetings'].docs.
         select{|g| g.data.keys.include?('location') }.
-        collect{|m| m['location']['slug'] }.compact.uniq
-
+        collect{|m| m.data.dig('location','slug') }.compact.uniq
       collection.select{|location| locations.include? location.data['slug'] }
     end
 
