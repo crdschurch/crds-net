8e9c860ef6454c5e1428e9a0be3b6523c6608cf7
silasbaur <silasbaur@gmail.com>
2020-04-01 10:51:00 -0400

    US19168 Ctfassets - update build with sed script for ctfasset to imgix for images

--- bin/build-command.sh
+++ bin/build-command.sh
@@ -8,6 +8,7 @@
   bundle exec jekyll crds &&
   bundle exec jekyll contentful --sites www.crossroads.net -f &&
   bundle exec jekyll build -- --update-search-index &&
+  find . -type f -name "*html" -print0 | xargs -0 sed -i '' -E 's/\/\/images.ctfassets.net\/y3a9myzsdjan/\/\/crds-media.imgix.net/g' &&
   ./bin/prerenderio-bust.sh &&
   ./bin/health-check.sh "we are crossroads" &&
   ./bin/kickOffCypress.sh
