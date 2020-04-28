05d686536d00c4a756110a7f4b0137e3211855de
tcmacdonald <tcmacdonald@gmail.com>
2020-04-28 10:30:55 -0400

    F1132 Rebrand Specify src for all components

--- _plugins/hooks/config.rb
+++ _plugins/hooks/config.rb
@@ -4,4 +4,13 @@ Jekyll::Hooks.register :site, :after_init do |site|
   else
     site.config['components_url'] = "https://#{ENV['CRDS_COMPONENTS_ENDPOINT'] || "components-int.crossroads.net"}/dist"
   end
+
+  env = case JEKYLL_ENV
+    when 'int' then 'int'
+    when 'demo' then 'demo'
+    else 'prod'
+  end
+
+  site.config['data_host'] = "https://#{ENV['CRDS_DATA_ENDPOINT'] || "crds-data.crossroads.net"}"
+  site.config['data_file'] = "#{env}.json"
 end
