9f34f9b3028d6508b8ba8a21feb0ab1fc7d91c7b
Taylor C. MacDonald <taylor@helloample.com>
2020-08-24 08:45:25 -0400

    DEXXXX Health Check Cleanup

--- _plugins/netlify-plugin-health-check/index.js
+++ _plugins/netlify-plugin-health-check/index.js
@@ -1,11 +1,11 @@
 module.exports = {
   async onPostBuild({ utils: { run, build } }) {
     try {
-      let idxCheck = await run("./bin/health-check.sh", [
+      await run("./bin/health-check.sh", [
         "_site/index.html",
         "we are crossroads",
       ]);
-      let redirCheck = await run("./bin/health-check.sh", [
+      await run("./bin/health-check.sh", [
         "_site/_redirects",
         "/*	/404.html	404",
       ]);
