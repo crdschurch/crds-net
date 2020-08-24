cc34185305abdefd5cc1c2e9ea2bc39464470526
Taylor C. MacDonald <taylor@helloample.com>
2020-08-24 08:27:40 -0400

    DEXXXX Health Check Refactor health check script for multiple arguments

--- bin/health-check.sh
+++ bin/health-check.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 set -e
-PATH_TO_FILE='./_site/index.html'
 
 if [ $# -eq 0 ]
 then
+  echo "Please specify file to search"
+else
+  PATH_TO_FILE=$1
+fi
+
+if [ $# -eq 1 ]
+then
   echo "Please specify argument containing string to search for."
 else
-  NEEDLE=$1
+  NEEDLE=$2
 fi
 
 if grep -Fq "$NEEDLE" $PATH_TO_FILE
 then
+  echo "\"${NEEDLE}\" is present in ${PATH_TO_FILE}"
   exit 0
 else
-  echo "${NEEDLE} not found in ${PATH_TO_FILE}"
+  echo "\"${NEEDLE}\" not found in ${PATH_TO_FILE}"
   exit 1
 fi
