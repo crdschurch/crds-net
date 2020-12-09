431efca5fd09416c9e8bca9aee644e1f536e93ac
janssejl <ll>
2020-12-09 09:52:03 -0500

    changing redirects to be inserted after 15th row in redirects.csv

--- lib/crds/redirects.rb
+++ lib/crds/redirects.rb
@@ -39,8 +39,8 @@ class Redirects
 
   def to_csv!(path = './redirects.csv', debug=true)
     rows = CSV.read(path)
-    rows.insert(3, *redirects)
-    rows.insert(3, *auth_required)
+    rows.insert(15, *redirects)
+    rows.insert(15, *auth_required)
     File.write(path, rows.map(&:to_csv).join)
     if debug
       puts "\nWrote #{redirects.size + auth_required.size} redirects from Contentful to #{path}"
