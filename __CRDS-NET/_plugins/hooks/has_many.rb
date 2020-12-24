d5aa83ff866b5dea12beb7baf0cb0151c3ce9415
Dan Holloran <dan@helloample.com>
2020-12-24 11:04:05 -0500

    Removes __CRDS-NET/_plugins
    Signed-off-by: Dan Holloran <dan@helloample.com>

--- __CRDS-NET/_plugins/hooks/has_many.rb
+++
@@ -1,6 +0,0 @@
-require 'active_support'
-require_relative '../../lib/crds/has_many'
-
-Jekyll::Hooks.register(:site, :post_read) do |site|
-  ::CRDS::HasMany.new(site).run!
-end
--- __CRDS-NET/_plugins/netlify-plugin-contentful-redirects/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  async onPreBuild({ utils: { run } }) {
-    await run("mkdir", ["-p", "./tmp"]);
-    await run("cp", ["./redirects.csv", "./tmp/.redirects_cache"]);
-    await run("./bin/contentful-redirects", ["./tmp/.redirects_cache"]);
-  },
-};
