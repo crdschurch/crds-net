5cf2b43d4dcb1f9b1d1fb7727a3faa19cc5b09b2
Dan Holloran <dan@helloample.com>
2020-12-24 10:07:12 -0500

    Cleans up old stylesheet directory and a few other config files
    Signed-off-by: Dan Holloran <dan@helloample.com>

--- __CRDS-NET/_assets/stylesheets/crds-styles/lib/crds-styles/version.rb
+++
@@ -1,8 +0,0 @@
-require 'json'
-
-module CRDS
-  module Styles
-    package_file = File.expand_path('../../package.json', __dir__)
-    VERSION = JSON.parse(File.read(package_file))['version']
-  end
-end
