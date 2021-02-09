2afe53d3c7896532decd97b60705a0c43c5f8d62
Silas Baur <silasbaur@gmail.com>
2021-02-09 13:25:28 -0500

    US20403 Merge Net and Media - revert Disables homepage health check
    Signed-off-by: Silas Baur <silasbaur@gmail.com>

--- _plugins/netlify-plugin-health-check/index.js
+++ _plugins/netlify-plugin-health-check/index.js
@@ -1,10 +1,10 @@
 module.exports = {
   async onPostBuild({ utils: { run, build } }) {
     try {
-      // await run("./bin/health-check.sh", [
-      //   "_site/index.html",
-      //   "we are crossroads",
-      // ]);
+      await run("./bin/health-check.sh", [
+        "_site/index.html",
+        "we are crossroads",
+      ]);
       await run("./bin/health-check.sh", [
         "_site/_redirects",
         "/*	/404.html	404",
