9c0cf141fe3d6957a45c2bda6b1e556f05c1d6e2
Silas Baur <silasbaur@gmail.com>
2021-02-10 10:49:24 -0500

    add trace to build command
    Signed-off-by: Silas Baur <silasbaur@gmail.com>

--- bin/build-command.sh
+++ bin/build-command.sh
@@ -3,7 +3,7 @@
 {
   bundle exec jekyll contentful --sites www.crossroads.net -f &&
   bundle exec rspec ./vendor ./spec &&
-  bundle exec jekyll build -- --update-search-index &&
+  bundle exec jekyll build -- --update-search-index --trace &&
   find . -type f -name "*html" -print0 | xargs -0 sed -i '' -E 's/\/\/images.ctfassets.net\/y3a9myzsdjan/\/\/crds-media.imgix.net/g' &&
   bundle exec ruby ./lib/crds/monetate.rb &&
   ./bin/prerenderio-bust.sh &&
