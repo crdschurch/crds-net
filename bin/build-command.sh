342fa96c910fb8c014d00290958fac771a285925
Silas Baur <silasbaur@gmail.com>
2021-03-16 10:18:20 -0400

    update build command with sed script
    Signed-off-by: Silas Baur <silasbaur@gmail.com>

--- bin/build-command.sh
+++ bin/build-command.sh
@@ -2,5 +2,6 @@
 # Ships logs to logz.io
 {
   bundle exec jekyll contentful --sites www.crossroads.net -f &&
-  bundle exec jekyll build
+  bundle exec jekyll build &&
+  find ./_site -type f -name "*html" -print0 | xargs -0 sed -i '' -E 's/\/\/images.ctfassets.net\/y3a9myzsdjan/\/\/crds-media.imgix.net/g' 
 }
