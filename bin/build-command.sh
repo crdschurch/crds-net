dd42b6bd5967171bf1f24b56552ad0df3a91a135
janssejl <ll>
2021-03-31 17:38:50 -0400

    removing excess build stuffs

--- bin/build-command.sh
+++ bin/build-command.sh
@@ -3,7 +3,4 @@
 {
   bundle exec jekyll contentful --sites www.crossroads.net -f &&
   bundle exec jekyll build --incremental &&
-  ./bin/prerenderio-bust.sh &&
-  ./bin/kickOffCypress.sh
-} 2>buildlog.txt
-./bin/logzio.sh
+} 
