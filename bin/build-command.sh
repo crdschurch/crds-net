38e4026519004ac55e63a8126e670029abe2364c
janssejl <ll>
2021-03-31 17:55:21 -0400

    Revert "removing excess build stuffs"
    This reverts commit dd42b6bd5967171bf1f24b56552ad0df3a91a135.

--- bin/build-command.sh
+++ bin/build-command.sh
@@ -3,4 +3,7 @@
 {
   bundle exec jekyll contentful --sites www.crossroads.net -f &&
   bundle exec jekyll build --incremental &&
-} 
+  ./bin/prerenderio-bust.sh &&
+  ./bin/kickOffCypress.sh
+} 2>buildlog.txt
+./bin/logzio.sh
