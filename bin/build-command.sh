0bbc6a3d23508d7a295e435670f151d96e179deb
janssejl <ll>
2021-03-31 18:02:08 -0400

    removing netlify caching plugin as it is not needed

--- bin/build-command.sh
+++ bin/build-command.sh
@@ -2,8 +2,5 @@
 # Ships logs to logz.io
 {
   bundle exec jekyll contentful --sites www.crossroads.net -f &&
-  bundle exec jekyll build --incremental &&
-  ./bin/prerenderio-bust.sh &&
-  ./bin/kickOffCypress.sh
-} 2>buildlog.txt
-./bin/logzio.sh
+  bundle exec jekyll build 
+}
