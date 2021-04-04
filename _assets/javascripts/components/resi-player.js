15ef8542264ff55c790b6cb04ef2b95d50d81622
Kell G <kell.gauthier@gmail.com>
2021-04-04 08:25:17 -0400

    resolves sunday service stream

--- _assets/javascripts/components/resi-player.js
+++ _assets/javascripts/components/resi-player.js
@@ -22,29 +22,21 @@ const isDayOfTheWeek = (day) => {
   }
 };
 
-const isServiceTime = () => {
+const isSundayServiceTime = () => {
   let isSunday = isDayOfTheWeek(0);
-  let isSaturday = isDayOfTheWeek(6);
-
-  let saturdayServiceTimes = (
-    (getEstTime() >= 1555 && getEstTime() <= 1710) || 
-    (getEstTime() >= 1725 && getEstTime() <= 1840)
-  );
-
   let sundayServiceTimes = (
     (getEstTime() >= 825 && getEstTime() <= 940) || 
     (getEstTime() >= 955 && getEstTime() <= 1110) ||
     (getEstTime() >= 1140 && getEstTime() <= 1255)
   );
-
-  return (isSunday || isSaturday) && (saturdayServiceTimes || sundayServiceTimes);
+  
+  return isSunday && sundayServiceTimes;
 };
 
-const refreshPageForServiceStart = (hours, minutes, seconds) => {
+const refreshPageForSundayServiceStart = (hours, minutes, seconds) => {
   let isSunday = isDayOfTheWeek(0);
-  let isSaturday = isDayOfTheWeek(6);
 
-  if ((!isSunday || !isSaturday) || !document.getElementById('location-page')) {
+  if (!isSunday || !document.getElementById('location-page')) {
     return;
   }
 
@@ -63,17 +55,12 @@ const refreshPageForServiceStart = (hours, minutes, seconds) => {
   }, timeout);
 };
 
-if (!isServiceTime() && document.getElementById('resi-player')) {
+if (!isSundayServiceTime() && document.getElementById('resi-player')) {
   document.getElementById('resi-player').remove();
 }
 
-if (isDayOfTheWeek(6)) {
-  refreshPageForServiceStart(15,55,1);
-  refreshPageForServiceStart(17,25,1);
-}
-
 if (isDayOfTheWeek(0)) {
-  refreshPageForServiceStart(8,25,1);
-  refreshPageForServiceStart(9,55,1);
-  refreshPageForServiceStart(11,40,1);
+  refreshPageForSundayServiceStart(8,25,1);
+  refreshPageForSundayServiceStart(9,55,1);
+  refreshPageForSundayServiceStart(11,40,1);
 }
