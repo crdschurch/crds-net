5c165c1b5b8fbd2dcc22b3cfbd6009050c74492d
Lynch <llynch@callibrity.com>
2021-06-01 11:42:33 -0400

    add media audio test back in

--- cypress/integration/404/404_spec.js
+++ cypress/integration/404/404_spec.js
@@ -1,25 +1,31 @@
 describe('Testing the 404 page:', function() {
   before(function() {
-    const uncaughtExceptionError = /.*> Cannot set property 'status' of undefined*/;
-    cy.ignoreMatchingErrors([uncaughtExceptionError]);
+    const importDeclarationsError = /.*import declarations may only appear at top level of a module.*/;
+    cy.ignoreMatchingErrors([importDeclarationsError]);
     cy.visit('/404');
   });
+
   it('/404 should have the header, footer and search button', function() {
     cy.get('crds-shared-header').should('exist');
     cy.get('crds-shared-footer').should('exist');
     cy.get('[data-automation-id="404-search-button"]').as('404SearchButton')
       .should('exist').and('be.visible');
   });
+
   it('/search page should load with search input when the search button is clicked', function() {
   cy.get('[data-automation-id="404-search-button"]').click();
+
   cy.get('.ais-SearchBox-input').as('searchField')
      .should('exist').and('be.visible');
+ 
   });  
 });
+
 describe('Testing invalid routes serve the expected 404 page:', () => {
   ['/notapage', '/live/notapage'].forEach((slug) => {
     it(`crossroads.net${slug}`, () => {
       cy.visit(slug, { failOnStatusCode: false });
+
       cy.get('crds-shared-header').should('exist');
       cy.get('crds-shared-footer').should('exist');
       cy.get('[data-automation-id="404-search-button"]').as('404SearchButton')
