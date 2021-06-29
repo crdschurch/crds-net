d184219d52172a4a8a28b781347938cc3c5084ad
Maria Szubski <hello@mariaszubski.com>
2021-06-29 16:02:06 -0400

    Prevent jumbotron bg video from loading on mobile

--- _assets/javascripts/components/jumbotron-video.js
+++ _assets/javascripts/components/jumbotron-video.js
@@ -1,5 +1,24 @@
 (function() {
   window.$(document).ready(function() {
     new window.CRDS.JumbotronVideos();
+
+    // Prevent background video from loading on mobile
+    const mediaQuery = window.matchMedia('(min-width: 480px)');
+    mediaQuery.addEventListener('change', handleScreenChange);
+
+    // Check screen size on load
+    handleScreenChange(mediaQuery);
+
+    function handleScreenChange(e) {
+      var bgVideo = document.getElementById('jumbotron-bg-video');
+
+      if (e.matches) {
+        bgVideo.setAttribute('src', bgVideo.getAttribute('data-src'));
+      } else {
+        if (!bgVideo.hasAttribute('data-src')) {
+          bgVideo.setAttribute('data-src', bgVideo.getAttribute('src'));
+        }
+      }
+    }
   });
 })();
