485529541e2c0508dffa3fd75448661e39ee6671
Maria Szubski <hello@mariaszubski.com>
2021-06-30 16:08:10 -0400

    Scope jumbotron media query to pages with a bg video element

--- _assets/javascripts/components/jumbotron-video.js
+++ _assets/javascripts/components/jumbotron-video.js
@@ -2,22 +2,22 @@
   window.$(document).ready(function() {
     new window.CRDS.JumbotronVideos();
 
-    // Prevent background video from loading on mobile
-    const mediaQuery = window.matchMedia('(min-width: 480px)');
-    mediaQuery.addEventListener('change', handleScreenChange);
+    // ---------------------------------------- Prevent bg video from loading on mobile
+    const bgVideo = document.getElementById('jumbotron-bg-video');
 
-    // Check screen size on load
-    handleScreenChange(mediaQuery);
+    if (bgVideo) {
+      const mediaQuery = window.matchMedia('(min-width: 480px)');
+      mediaQuery.addEventListener('change', handleScreenChange);
+
+      // Check screen size on load
+      handleScreenChange(mediaQuery);
+    }
 
     function handleScreenChange(e) {
-      var bgVideo = document.getElementById('jumbotron-bg-video');
-
       if (e.matches) {
         bgVideo.setAttribute('src', bgVideo.getAttribute('data-src'));
-      } else {
-        if (!bgVideo.hasAttribute('data-src')) {
-          bgVideo.setAttribute('data-src', bgVideo.getAttribute('src'));
-        }
+      } else if (!bgVideo.hasAttribute('data-src')) {
+        bgVideo.setAttribute('data-src', bgVideo.getAttribute('src'));
       }
     }
   });
