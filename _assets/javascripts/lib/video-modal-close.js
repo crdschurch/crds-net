82d03a8c32337663adfba0d8058e2410eb558edd
Austin Engen <austin@helloample.com>
2021-08-24 15:02:05 -0400

    add stop video logic

--- _assets/javascripts/lib/video-modal-close.js
+++ _assets/javascripts/lib/video-modal-close.js
@@ -1,3 +1,13 @@
-$("#videoModal").on('hidden.bs.modal', function (e) {
-    $("#videoModal iframe").attr("src", $("#videoModal iframe").attr("src"));
+$('#videoModal').on('hidden.bs.modal', function(e) {
+  $('#videoModal iframe').attr('src', $('#videoModal iframe').attr('src'));
+});
+
+var video = document.getElementById('modalVideo');
+function stopVideo() {
+  video.pause();
+  video.currentTime = 0;
+}
+
+$('.stop-video').on('click', function() {
+  stopVideo();
 });
