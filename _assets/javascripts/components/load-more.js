fbe46eb9192e9b4336a195f61f511b6587e85e0c
Michael Vezzani <mjvezzani@gmail.com>
2021-11-03 06:16:34 -0600

    US21759 Shows Pagination
    Working implementation of pagination on shows, separated by season

---
+++ _assets/javascripts/components/load-more.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  function addToDisplayedEpisodes(episodes) {
+    let displayedEpisodes = [];
+    for ( let i = 0; i < episodes.length; i++ ) {
+      if (!episodes[i].classList.contains('hide-episode')) {
+        displayedEpisodes.push(episodes[i]);
+      }
+    }
+    return displayedEpisodes;
+  }
+
+  function revealMoreEpisodes(e, episodes) {
+    let allEpisodes = document.querySelectorAll(`[data-${e.target.id.slice(0, 8)}]`)
+    let displayedEpisodes = addToDisplayedEpisodes(allEpisodes);
+  
+    for ( let i = displayedEpisodes.length; i < displayedEpisodes.length + 5; i++) {
+      if (undefined == episodes[i]) {
+        return
+      }
+
+      episodes[i].classList.remove("hide-episode");
+
+      if (undefined == episodes[i+1]) {
+        document.getElementById(e.target.id).classList.add('disabled');
+      }
+    }
+  }
+
+  function initializeLoadMoreButtons() {
+    const totalNumberOfSeasons = document.getElementsByTagName("crds-tabs")[0].attributes.tabs.textContent.replace('\\', '').split(',').length;
+    let episodesBySeason = {}
+
+    for ( let i = 0; i < totalNumberOfSeasons; i++ ) {
+      let seasonNumber = `${i + 1}`;
+      episodesBySeason[seasonNumber] = document.querySelectorAll(`[data-season-${seasonNumber}]`);
+      const loadMoreButton = document.getElementById(`season-${seasonNumber}-load-more`);
+      loadMoreButton.addEventListener("click", function(e) {
+        revealMoreEpisodes(e, episodesBySeason[seasonNumber]);
+      }, false);
+
+      loadMoreButton.click();
+    }
+  }
+
+  initializeLoadMoreButtons();
+
+
+});
