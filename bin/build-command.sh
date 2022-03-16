fdd4eb8ed32bbcb6d4b5083b76fa14ee35e45877
Taylor MacDonald <tcmacdonald@gmail.com>
2022-03-16 15:35:52 -0400

    Rescue build errors from prerenderio script

--- bin/build-command.sh
+++ bin/build-command.sh
@@ -3,7 +3,7 @@
 {
   bundle exec jekyll contentful --sites www.crossroads.net -f &&
   bundle exec jekyll build -- --update-search-index &&
-  # ./bin/prerenderio-bust.sh &&
+  ./bin/prerenderio-bust.sh &&
   ./bin/kickOffCypress.sh &&
   ./bin/adjustCrossroadsOnlineCanonicalURL.sh
 } 2>buildlog.txt
--- bin/prerenderio-bust.sh
+++ bin/prerenderio-bust.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST ${CACHEBUST_ENDPOINT}
+curl -X POST ${CACHEBUST_ENDPOINT} 2>/dev/null || true
