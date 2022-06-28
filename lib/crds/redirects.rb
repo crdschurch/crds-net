4c00b95ec9407d5919836b23e19d769285521d45
Taylor C. MacDonald <taylor@helloample.com>
2022-06-28 16:37:12 -0400

    Insert CMS redirects _after_ rules defined in codebase

--- lib/crds/redirects.rb
+++ lib/crds/redirects.rb
@@ -39,8 +39,9 @@ class Redirects
 
   def to_csv!(path = './redirects.csv', debug=true)
     rows = CSV.read(path)
-    rows.insert(18, *redirects)
-    rows.insert(18, *auth_required)
+    n = rows.length - 1
+    rows.insert(n, *redirects)
+    rows.insert(n, *auth_required)
     File.write(path, rows.map(&:to_csv).join)
     if debug
       puts "\nWrote #{redirects.size + auth_required.size} redirects from Contentful to #{path}"
