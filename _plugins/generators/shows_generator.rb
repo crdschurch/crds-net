39f8fe4fbb53ecb87308be58616eb6e2d5b38df2
Silas Baur <silasbaur@gmail.com>
2022-09-21 18:17:35 -0400

    clean up
    Signed-off-by: Silas Baur <silasbaur@gmail.com>

--- _plugins/generators/shows_generator.rb
+++ _plugins/generators/shows_generator.rb
@@ -5,6 +5,7 @@ module ShowsGenerator
       videos = site.collections['videos'].docs
       shows = site.collections['videos'].docs.select{ |doc| doc.data.dig('collections') }
       shows.map do |show|
+        
         show.data['collections'] = show.data['collections'].map do |collection|
           id = collection['id']
           site.collections['collections'].docs.detect{ |doc| doc.data.dig('contentful_id') == id}
