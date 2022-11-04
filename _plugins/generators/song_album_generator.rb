beb65aedde40c613097bcf358056bae5f948f188
bbbtttiii <ben.iburg@helloample.com>
2022-11-04 16:48:18 -0500

    Generate songs from albums

---
+++ _plugins/generators/song_album_generator.rb
@@ -0,0 +1,16 @@
+module GetAlbumSongs
+  class Generator < Jekyll::Generator
+    def generate(site)
+      albums = site.collections['albums'].docs
+      songs = site.collections['songs'].docs
+      song_ids = Hash[albums.collect do |album|
+        [album.data['slug'], album.data['songs'].collect{|song| song['id']}]
+      end]
+      songs.each do |song|
+        slug = song_ids.keys.detect{|id| song_ids[id].include?(song.data['id'])}
+        song.data['album_slug'] = slug
+        song.data['album_url'] = "#{ENV['CRDS_MUSIC_ENDPOINT'].chomp('/')}/music/#{slug}/#{song.data['slug']}"
+      end
+    end
+  end
+end
