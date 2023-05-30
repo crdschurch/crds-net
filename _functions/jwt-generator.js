8fc6ace74709c3a7a79d0128ec38619e2c6530d6
Taylor MacDonald <tcmacdonald@gmail.com>
2023-05-30 18:01:57 -0400

    Refactor for CORS headers

--- _functions/jwt-generator.js
+++ _functions/jwt-generator.js
@@ -2,11 +2,11 @@
 "use strict";
 const JWT = require("jsrsasign");
 
-exports.handler = () => {
+exports.handler = (_event, _context, callback) => {
   const oHeader = { alg: "HS256", typ: "JWT" };
   const sHeader = JSON.stringify(oHeader);
-  var myDate = new Date();
-  myDate.setHours(myDate.getHours() + 2160); // 90 days
+  var expiration = new Date();
+  expiration.setHours(expiration.getHours() + 2160); // 90 days
 
   const sPayload = JSON.stringify({
     app_metadata: {
@@ -14,15 +14,25 @@ exports.handler = () => {
         roles: ["user"],
       },
     },
-    exp: Math.floor(myDate.getTime() / 1000),
+    exp: Math.floor(expiration.getTime() / 1000),
   });
 
-  const token = JWT.jws.JWS.sign(
+  const body = JWT.jws.JWS.sign(
     "HS256",
     sHeader,
     sPayload,
     process.env.JWT_SECRET
   );
 
-  return { statusCode: 200, body: token };
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+  };
+
+  return callback(null, {
+    statusCode: 200,
+    body,
+    headers,
+  });
 };
