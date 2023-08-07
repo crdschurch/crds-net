c321529ccf51ff7264505ac6a4d4e9d68b4f8a06
Josh Lane <joshuadlane85@gmail.com>
2023-08-07 16:08:14 -0400

    update source for components

--- _plugins/hooks/config.rb
+++ _plugins/hooks/config.rb
@@ -2,7 +2,7 @@ Jekyll::Hooks.register :site, :after_init do |site|
   if ENV['NETLIFY']
     site.config['components_endpoint'] = "/components"
   else
-    site.config['components_endpoint'] = "https://#{ENV['CRDS_COMPONENTS_ENDPOINT'] || "components-int.crossroads.net"}/dist"
+    site.config['components_endpoint'] = "https://#{ENV['CRDS_COMPONENTS_ENDPOINT'] || "components-demo.crossroads.net"}/dist"
   end
 
   env = case ENV['JEKYLL_ENV']
