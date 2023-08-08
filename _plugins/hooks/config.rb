d5d33b96ca01a850742072df9ee3a0f4aa0483a4
Josh Lane <joshuadlane85@gmail.com>
2023-08-08 10:37:51 -0400

    testing this upstream

--- _plugins/hooks/config.rb
+++ _plugins/hooks/config.rb
@@ -1,6 +1,7 @@
 Jekyll::Hooks.register :site, :after_init do |site|
   if ENV['NETLIFY']
     site.config['components_endpoint'] = "/components"
+    site.config['netlify'] = "#{ENV['NETLIFY']}"
   else
     site.config['components_endpoint'] = "https://#{ENV['CRDS_COMPONENTS_ENDPOINT'] || "components-demo.crossroads.net"}/dist"
   end
