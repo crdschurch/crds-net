ce3b1ff0db3614c3e93b5d523846dc0cf839c370
Josh Lane <joshuadlane85@gmail.com>
2023-08-11 20:22:10 -0400

    remove testing configurations

--- _plugins/hooks/config.rb
+++ _plugins/hooks/config.rb
@@ -1,7 +1,6 @@
 Jekyll::Hooks.register :site, :after_init do |site|
   if ENV['NETLIFY'] && ENV['CRDS_ENV'] === 'prod'
     site.config['components_endpoint'] = "/components"
-    site.config['netlify'] = "#{ENV['NETLIFY']}"
   else
     site.config['components_endpoint'] = "https://#{ENV['CRDS_COMPONENTS_ENDPOINT'] || "components-demo.crossroads.net"}/dist"
   end
