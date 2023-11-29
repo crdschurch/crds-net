5662b9f52c9537451d70059b4ede5b1e25bcc6ef
bbbtttiii <ben.iburg@helloample.com>
2023-11-28 17:01:37 -0700

    Fetch livestreams

---
+++ _plugins/hooks/livestreams.rb
@@ -0,0 +1,19 @@
+Jekyll::Hooks.register :site, :pre_render do |site|
+  require 'open-uri'
+  require 'json'
+
+  data_endpoint = "https://#{ENV['CRDS_DATA_ENDPOINT'] || 'crds-data.crossroads.net'}"
+  livestream_url = "#{data_endpoint}/livestreams/#{ENV['CRDS_ENV']}.json"
+
+  begin
+    response = Net::HTTP.get(URI(livestream_url))
+    data = JSON.parse(response)
+
+    site.data['livestreams'] = data['livestreams']
+
+    Jekyll.logger.info "Livestreams data: #{site.data['livestreams']}"
+  rescue => e
+    Jekyll.logger.warn "Error fetching livestreams data: #{e.message}"
+    site.data['livestreams'] = {}
+  end
+end
