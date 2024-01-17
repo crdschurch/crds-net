6c4584a596d87073f73d9fb03d5cc1e3d4b73e12
Jake Tellep <jacobtellep@Jakes-MacBook-Pro.local>
2024-01-17 07:14:39 -0500

    remove prod conditional

--- _plugins/hooks/config.rb
+++ _plugins/hooks/config.rb
@@ -1,9 +1,5 @@
 Jekyll::Hooks.register :site, :after_init do |site|
-  if ENV['NETLIFY'] && ENV['CRDS_ENV'] === 'prod'
-    site.config['components_endpoint'] = "/components"
-  else
-    site.config['components_endpoint'] = "https://#{ENV['CRDS_COMPONENTS_ENDPOINT'] || "components-demo.crossroads.net"}/dist"
-  end
+  site.config['components_endpoint'] = "https://#{ENV['CRDS_COMPONENTS_ENDPOINT'] || "components-demo.crossroads.net"}/dist"
 
   env = case ENV['JEKYLL_ENV']
     when 'int' then 'int'
