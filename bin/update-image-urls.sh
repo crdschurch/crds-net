841973485f34dd682cb4843cd1aceb786122f0e7
bbbtttiii <ben.iburg@helloample.com>
2024-05-06 08:25:48 -0600

    Backmerge chat button + image fix

--- bin/update-image-urls.sh
+++ bin/update-image-urls.sh
@@ -6,10 +6,10 @@ replace_image_urls() {
   if [[ "$file" =~ \.html$ ]]; then
     if [[ "$OSTYPE" == "darwin"* ]]; then
       sed -E -i '' -e 's#(https?:)?//images\.ctfassets\.net/[^/]+/([^/]+)/([^/]+)/([^/#?]+(\.png|\.PNG|\.jpe?g|\.JPE?G))#https://crds-media.imgix.net/\2/\3/\4#g' "$file"
-      sed -E -i '' -e 's#(https?://crds-media\.imgix\.net/[^"'\''?]+)(['\'')])#\1?auto=compress\2#g' "$file"
+      sed -E -i '' -e 's#(https?://crds-media\.imgix\.net/[^"]+)#\1?auto=compress#g' "$file"
     else
       sed -E -i -e 's#(https?:)?//images\.ctfassets\.net/[^/]+/([^/]+)/([^/]+)/([^/#?]+(\.png|\.PNG|\.jpe?g|\.JPE?G))#https://crds-media.imgix.net/\2/\3/\4#g' "$file"
-      sed -E -i -e 's#(https?://crds-media\.imgix\.net/[^"'\''?]+)(['\'')])#\1?auto=compress\2#g' "$file"
+      sed -E -i -e 's#(https?://crds-media\.imgix\.net/[^"]+)#\1?auto=compress#g' "$file"
     fi
   fi
 }
