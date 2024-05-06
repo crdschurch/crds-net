5b6fbecb81c9461f120430bf6daa13056fed572a
bbbtttiii <ben.iburg@helloample.com>
2024-05-06 10:49:14 -0600

    revert script again

--- bin/update-image-urls.sh
+++ bin/update-image-urls.sh
@@ -5,15 +5,11 @@ replace_image_urls() {
 
   if [[ "$file" =~ \.html$ ]]; then
     if [[ "$OSTYPE" == "darwin"* ]]; then
-      # Replace image domain and update the path format
       sed -E -i '' -e 's#(https?:)?//images\.ctfassets\.net/[^/]+/([^/]+)/([^/]+)/([^/#?]+(\.png|\.PNG|\.jpe?g|\.JPE?G))#https://crds-media.imgix.net/\2/\3/\4#g' "$file"
-      # Correctly append auto=compress based on whether the URL already has query parameters
-      sed -E -i '' -e 's#(https?://crds-media\.imgix\.net/[^"'\'']*)(\?[^"'\'' ]*)?(['\'')])#\1\2\3&auto=compress#g' -e 's#(&auto=compress)(\?[^"'\'' ]*)#?auto=compress\2#g' "$file"
+      sed -E -i '' -e 's#(https?://crds-media\.imgix\.net/[^"'\''?]+)(['\'')])#\1?auto=compress\2#g' "$file"
     else
-      # Replace image domain and update the path format
       sed -E -i -e 's#(https?:)?//images\.ctfassets\.net/[^/]+/([^/]+)/([^/]+)/([^/#?]+(\.png|\.PNG|\.jpe?g|\.JPE?G))#https://crds-media.imgix.net/\2/\3/\4#g' "$file"
-      # Correctly append auto=compress based on whether the URL already has query parameters
-      sed -E -i -e 's#(https?://crds-media\.imgix\.net/[^"'\'']*)(\?[^"'\'' ]*)?(['\'')])#\1\2\3&auto=compress#g' -e 's#(&auto=compress)(\?[^"'\'' ]*)#?auto=compress\2#g' "$file"
+      sed -E -i -e 's#(https?://crds-media\.imgix\.net/[^"'\''?]+)(['\'')])#\1?auto=compress\2#g' "$file"
     fi
   fi
 }
