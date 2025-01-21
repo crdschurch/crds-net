f4e32ea1fc7f70e7784c36cafacaff2c2d036f80
Jake Tellep <jacobtellep@Jakes-MacBook-Pro.local>
2025-01-21 11:03:38 -0500

    import new library conditionally

--- _plugins/hooks/config.rb
+++ _plugins/hooks/config.rb
@@ -1,5 +1,7 @@
 Jekyll::Hooks.register :site, :after_init do |site|
   site.config['components_endpoint'] = "https://#{ENV['CRDS_COMPONENTS_ENDPOINT'] || "components.crossroads.net"}/dist"
+  site.config['unified_components_endpoint'] = "https://#{ENV['CRDS_UNIFIED_COMPONENTS_ENDPOINT'] || "components.crossroads.net"}/dist/unified-components.js"
+  site.config['use_unified_components'] = ENV['USE_UNIFIED_COMPONENTS'] == 'true'
 
   env = case ENV['JEKYLL_ENV']
     when 'int' then 'int'
